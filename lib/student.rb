cd7e03c078834bda7b0bca0fc41fcc1b0bc202aa
Jordan Kline <jordan.kline@gmail.com>
2018-01-11 19:04:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,10 +4,10 @@ attr_reader :knowledge
   def initialize
     @knowledge = []
   end
-
-  def knowledge
-    @knowledge
-  end
+  # 
+  # def knowledge
+  #   @knowledge
+  # end
 
   def learn(knowhow)
     self.knowledge << knowhow
